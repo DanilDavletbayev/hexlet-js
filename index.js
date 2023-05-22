032b8c31fa397668567012153459c433498c798b
Danil <davletbayev4@mail.ru>
2023-05-22 12:51:02 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
 
+// eslint-disable-next-line no-console
 console.log('Hello, Hexlet!');
+
+// eslint-disable-next-line no-console
 console.log(_.last(['one', 'two']));
 const fn = (a, b) => {
   const c = a + b;
   return c;
 };
 
+// eslint-disable-next-line no-console
 console.log(fn(1, 2));
